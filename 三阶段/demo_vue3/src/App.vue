0a75cbc454b48bb8d3daea97150bb55fdf7b9ac3
byebai95 <1757960028@qq.com>
2025-06-08 22:24:31 +0800

    vue 案例

--- 三阶段/demo_vue3/src/App.vue
+++ 三阶段/demo_vue3/src/App.vue
@@ -1,30 +1,24 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+    <div>
+        <h1>Hello World</h1>
+    </div>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+
+<script>
+
+</script>
+
+<style>
+h1 {
+    background-color: #ddd;
+    padding: 10px;
+    font-size: 20px;
+    font-weight: bold;
+    text-align: left;
+    border-radius: 5px;
+    margin: 10px;
+    box-shadow: 0 0 10px 0 rgba(0, 0, 0, 0.1);
+    transition: all 0.3s ease;
 }
 </style>
