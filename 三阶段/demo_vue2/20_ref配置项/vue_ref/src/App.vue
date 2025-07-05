5d3d3b183f6af355f45b98688e4213bee3550dcc
byebai95 <1757960028@qq.com>
2025-07-05 13:08:54 +0800

    脚手架vue cli 使用

---
+++ 三阶段/demo_vue2/20_ref配置项/vue_ref/src/App.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+        <SchoolStudent class="sch"  ref="stu"/>
+        <h5 ref="h5">组件名称：{{ name }}</h5>
+        <button @click="showName">Ref组件</button>
+        <button @click="showName2">Ref标签</button>
+    </div>
+</template>
+
+<script>
+import SchoolStudent from './components/SchoolStudent.vue'
+export default {
+    name: 'App',
+    components: {
+        SchoolStudent,
+    },
+    data() {
+        return {
+            name: "App"
+        }
+    },
+    methods: {
+        showName() {
+            console.log(this.$refs.stu)
+        },
+        showName2() {
+            console.log(this.$refs.h5)
+        }
+    }
+}
+</script>
+
+
+
+<style>
+.sch {
+    background-color: #f00;
+}
+</style>
