bc3bce2cd6ef2de06d304273e0eb3e3fa27677f0
byebai95 <1757960028@qq.com>
2025-07-05 13:46:09 +0800

    props属性

---
+++ 三阶段/demo_vue2/21_props配置项使用/vue_props/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <MyStudent :name="name" />
+    <hr>
+    <MyStudent :name="name" age="22" :sex="sex" />
+    <hr>
+  </div>
+</template>
+
+<script>
+import MyStudent from './components/MyStudent.vue'
+export default {
+  name: 'App',
+  components: {
+    MyStudent
+  },
+  data() {
+    return {
+      name: '张三',
+      sex: 'man'
+    }
+  }
+}
+</script>
+
+<style></style>
---
+++ 三阶段/demo_vue2/21_props配置项使用/vue_props/src/components/MyStudent.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <h2>学生姓名：{{ name }}</h2>
+    <h2>学生年龄：{{ age }}</h2>
+    <h2>学生性别：{{ sex }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'MyStudent',
+    // data() {
+    //     return {
+    //         name: '张三',
+    //         age: 18
+    //     }
+    // },
+    props:{
+      name: {
+        type: String,
+        required: true
+      },
+      age: {
+        type: Number,
+        required: false,
+        default: 18
+      },
+      sex: {
+        type: String,
+        default: '男'
+      }
+    }
+}
+</script>
+
+<style>
+
+</style>
