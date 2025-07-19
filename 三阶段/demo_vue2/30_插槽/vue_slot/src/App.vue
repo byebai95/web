b8cb3ec900c500ef58e8460f158758d660cdf237
byebai95 <1757960028@qq.com>
2025-07-19 16:35:21 +0800

    add slot

---
+++ 三阶段/demo_vue2/30_插槽/vue_slot/src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <div id="app">
+    <CategoryItem title="食物">
+      <img src="https://pic35.photophoto.cn/20150511/0034034892281415_b.jpg" alt="">
+    </CategoryItem>
+
+    <CategoryItem title="游戏">
+      <ul>
+        <li v-for="(item, index) in games" :key="index">{{ item }}</li>
+      </ul>
+    </CategoryItem>
+
+    <CategoryItem title="衣服">
+      <video controls src="https://vod.v.jstv.com/2025/07/09/JSTV_JSGGNEW_1752060650284_9ia62Nc_1678.mp4" alt="" />
+    </CategoryItem>
+  </div>
+</template>
+
+<script>
+import CategoryItem from './components/CategoryItem.vue'
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      food: ['apple', 'banana', 'cherry'],
+      games: ['lol', 'csgo', 'dota2'],
+      clothes: ['shirt', 'pants', 'shoes']
+    }
+  },
+  components: {
+    CategoryItem
+  }
+}
+</script>
+
+<style scoped>
+#app {
+  display: flex;
+  justify-content: space-around;
+}
+
+img {
+  width: 100%;
+}
+
+video {
+  width: 100%;
+}
+</style>
---
+++ 三阶段/demo_vue2/30_插槽/vue_slot/src/components/CategoryItem.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="category-item">
+        <h3>{{ title }}</h3>
+
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'CategoryItem',
+    props: {
+        list: {
+            type: Array,
+            required: true
+        },
+        title: {
+            type: String,
+            required: true
+        }
+    }
+}
+</script>
+
+<style scoped>
+.category-item {
+    width: 300px;
+    height: 400px;
+    background-color: lightgray;
+}
+
+h3 {
+    text-align: center;
+    background-color: yellow;
+}
+
+ul {
+    list-style: none;
+}
+</style>
